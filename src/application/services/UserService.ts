3d2a5363ab94cbb0edb62c9ef46ae78d06ed46d9
CANMERTINYO <38213551+c4nzin@users.noreply.github.com>
2026-04-08 20:35:29 +0300

    feat(backend): add user service

--- src/application/services/UserService.ts
+++ src/application/services/UserService.ts
@@ -1,11 +1,11 @@
 import { User } from "../../domain/entities/User";
-import { BadRequestError, NotFoundError } from "../../domain/errors/app-error";
+import { ConflictError, NotFoundError } from "../../domain/errors/app-error";
 import { IUserRepository } from "../../domain/interfaces/IUserRepository";
 
 export class UserService {
   constructor(private userRepository: IUserRepository) {}
 
-  async getUserById(id: string): Promise<User | NotFoundError> {
+  async getUserById(id: string): Promise<User> {
     const user = await this.userRepository.findById(id);
 
     if (!user) {
@@ -15,7 +15,7 @@ export class UserService {
     return user;
   }
 
-  async getUserByEmail(email: string): Promise<User | NotFoundError> {
+  async getUserByEmail(email: string): Promise<User> {
     const user = await this.userRepository.findByEmail(email);
 
     if (!user) {
@@ -25,11 +25,11 @@ export class UserService {
     return user;
   }
 
-  async createUser(user: User): Promise<User | BadRequestError> {
+  async createUser(user: User): Promise<User> {
     const existingUser = await this.userRepository.findByEmail(user.email);
 
     if (existingUser) {
-      throw new BadRequestError(`User with email ${user.email} already exists`);
+      throw new ConflictError(`User with email ${user.email} already exists`);
     }
 
     return this.userRepository.save(user);
